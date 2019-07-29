91ac815a056da032160f1add7f7c9eacd973018b
Jon Dotsoy <hi@jon.soy>
2019-07-29 01:49:03 -0400

    added iconfindersedan

--- pages/inicio.tsx
+++ pages/inicio.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import styled from "styled-components";
 import NavBarComponent from '../components/Navbar/NavBar.component';
 import ContainerComponent from '../components/Container';
 import Head from 'next/head';
 import ResetCSS from "../components/ResetCSS";
 import MenuNavComponent from "../components/MenuLateral/ContainerMenuLateral.component";
+import styled from 'styled-components';
+import IconfinderSedan285810Component from '../components/icons/IconfinderSedan285810.component';
+
+const Content = styled.div`
+  flex: 1;
+  display: block;
+`;
+
+const Box = styled.div`
+  margin: 20px auto;
+  max-width: 800px;
+`
 
 export default () => {
   return <>
@@ -17,6 +28,15 @@ export default () => {
 
     <ContainerComponent>
       <MenuNavComponent></MenuNavComponent>
+
+      <Content>
+        <Box>
+          <IconfinderSedan285810Component p={75}/>
+          <IconfinderSedan285810Component p={50}/>
+          <IconfinderSedan285810Component p={0}/>
+        </Box>
+      </Content>
+
     </ContainerComponent>
   </>;
 }
