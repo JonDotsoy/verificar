1e1c85af63fb702ecc79373a9af5c4db1dbe2074
Jon Dotsoy <hi@jon.soy>
2019-08-04 20:35:12 -0400

    Update menu and icons

--- components/MenuLateral/ContainerMenuLateral.component.tsx
+++ components/MenuLateral/ContainerMenuLateral.component.tsx
@@ -5,17 +5,20 @@ import BaselineTimelineCompoent from '../icons/BaselineTimeline.compoent';
 import BaselineTrending from '../icons/BaselineTrending.component';
 import BaselineBuild from '../icons/BaselineBuild.component';
 import BaselineMonetizationOn from '../icons/BaselineMonetizationOn.component';
+import Icons8VelocimetroComponent from '../icons/Icons8Velocimetro.component';
+import BaselineMenuComponent from '../icons/BaselineMenu.component';
 
 const Container = styled.div`
   background-color: black;
   display: inline-block;
-  min-width: 330px;
-  padding: 20px 0px;
+  min-width: 260px;
+  padding: 0px 0px;
 `;
 
 export default () =>
   <Container>
-    <ItemMenuComponent href="/inicio" Icon={BaselineTimelineCompoent} title="Inicio" actived={true} />
+    <ItemMenuComponent Icon={BaselineMenuComponent} title="Menu" />
+    <ItemMenuComponent href="/inicio" Icon={Icons8VelocimetroComponent} title="Inicio" />
     <ItemMenuComponent href="/siniestros" Icon={BaselineTimelineCompoent} title="Siniestros" />
     <ItemMenuComponent href="/liquidadores" Icon={BaselineTrending} title="Liquidadores" />
     <ItemMenuComponent href="/talleres" Icon={BaselineBuild} title="Talleres" />
