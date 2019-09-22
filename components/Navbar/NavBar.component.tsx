c9f8c99e93c203e8817d263abe0d778fee873720
Jon Dotsoy <hi@jon.soy>
2019-09-22 01:47:46 -0300

    Added menu notifications

--- components/Navbar/NavBar.component.tsx
+++ components/Navbar/NavBar.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OutlineSearch from '../icons/OutlineSearch.component';
 import OutlineNotifications from '../icons/OutlineNotifications.component';
 import OutlineSettings from '../icons/OutlineSettings.component';
@@ -11,6 +11,103 @@ const Container = styled.div`
   display: flex;
   flex-flow: row;
   justify-content: space-between;
+
+  .notification-content {
+    position: relative;
+    border: none;
+    background: none;
+    padding: 0px;
+    margin: 0px;
+
+    .notification-btn {
+      border: none;
+      padding: 0px;
+      margin: 0px;
+      background: none;
+    }
+
+    .notification-indicator {
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      background: rgb(253, 254, 0);
+      font-family: "Source Sans Pro", sans-serif;
+      width: 20px;
+      height: 20px;
+      font-size: 14px;
+      border-radius: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  .notification-box {
+    border-radius: 10px;
+    position: absolute;
+    width: 400px;
+    background-color: rgb(254, 140, 9);
+    right: -100px;
+    top: 60px;
+    box-shadow: 0px 3px 3px 0px #00000040;
+
+    .list-detail {
+      padding: 0px 20px;
+
+      .item-content:last-child {
+        border: none;
+      }
+    }
+
+    .item-content {
+      padding: 10px 20px;
+      color: white;
+      font-size: 14px;
+
+      display: flex;
+      border-bottom: solid 1px white;
+
+      .text {
+        flex: 1;
+      }
+
+      button {
+        
+      }
+    }
+
+    .header {
+      background-color: rgb(208, 113, 1);
+      border-radius: 10px;
+
+      .text {
+        text-transform: uppercase;
+      }
+
+      button {
+        border: none;
+        padding: 0px;
+        padding: 0px;
+        background: none;
+        color: white;
+        font-size: 14px;
+      }
+    }
+
+    &:before {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 0px;
+      height: 0px;
+      top: -20px;
+      right: 105px;
+      border-top: solid 0px transparent;
+      border-left: solid 15px transparent;
+      border-right: solid 15px transparent;
+      border-bottom: solid 20px rgb(208, 113, 1);
+    }
+  }
 `;
 
 const SectionLeft = styled.div`
@@ -41,6 +138,13 @@ const ItemMenu = styled.div`
 `;
 
 export default () => {
+  const [menuNotificationVisible, setMenuNotificationVisible] = useState(false);
+  const [countNotification, setCountNotification] = useState(8 as null | number);
+
+  function toggleMenuNotifications() {
+    setMenuNotificationVisible(e => !e);
+  }
+
   return <Background>
     <Container>
       <SectionLeft>
@@ -56,7 +160,38 @@ export default () => {
           </Link>
         </ItemMenu>
         <ItemMenu>
-          <OutlineNotifications></OutlineNotifications>
+          <div className="notification-content">
+            <button className="notification-btn" onClick={toggleMenuNotifications}>
+              <OutlineNotifications></OutlineNotifications>
+              {countNotification && <div className="notification-indicator"><span>{countNotification}</span></div>}
+            </button>
+
+            {menuNotificationVisible &&
+              <div className="notification-box">
+                <div className="item-content header">
+                  <div className="text">
+                    Notificaciones: 4
+                </div>
+                  <button>ver todos</button>
+                </div>
+
+                <div className="list-detail">
+                  <div className="item-content">
+                    <div className="text">Nuevo presupuesto: 4</div>
+                  </div>
+                  <div className="item-content">
+                    <div className="text">Sinistros Asignados: 20</div>
+                  </div>
+                  <div className="item-content">
+                    <div className="text">Presupuesto sin revisar: 2</div>
+                  </div>
+                  <div className="item-content">
+                    <div className="text">Vehiculo: VG3456 - Despachado</div>
+                  </div>
+                </div>
+              </div>
+            }
+          </div>
         </ItemMenu>
         <ItemMenu>
           <OutlineSettings></OutlineSettings>
