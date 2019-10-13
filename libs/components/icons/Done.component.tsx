9a6d67aa14037cbcad0018d94653bd063e34e875
Jon Dotsoy <hi@jon.soy>
2019-10-13 03:43:39 -0300

    Added util to list icons added icon DoneComponent

---
+++ libs/components/icons/Done.component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { SVGIconNavbarWhite } from "./BaseSVGIcon.component";
+
+export default ({ className }: { className?: string }) =>
+  <SVGIconNavbarWhite className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z" /><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" /></SVGIconNavbarWhite>
