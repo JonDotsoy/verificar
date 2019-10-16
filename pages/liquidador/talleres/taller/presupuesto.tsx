fcd06d2ac661ac5ad8cd674d4139ada201d67ff3
Jon Dotsoy <hi@jon.soy>
2019-10-16 14:54:56 -0300

    Fix ref menu taller

--- pages/liquidador/talleres/taller/presupuesto.tsx
+++ pages/liquidador/talleres/taller/presupuesto.tsx
@@ -7,6 +7,7 @@ import AlertHandDrawnSymbolComponent from "../../../../libs/components/icons/Ale
 import Carmechanic89096Component from "../../../../libs/components/icons/Carmechanic89096.component";
 import IconPricingComponent from "../../../../libs/components/icons/IconPricing.component";
 import { TitlePresupuesto } from "../../../../libs/components/TitlePresupuesto";
+import { MenuTaller } from "../../../../libs/components/MenuTaller";
 
 const Content = styled.div({});
 
@@ -20,16 +21,12 @@ export default () => {
 
       <div style={{ width: '100%' }}>
         <Content>
-          <ContainerMenuTop className="">
-            <BtnNavitagionSinister Icon={AlertHandDrawnSymbolComponent} text="Sinistro" href="/liquidador/talleres/taller/sinistro"></BtnNavitagionSinister>
-            <BtnNavitagionSinister Icon={Carmechanic89096Component} text="Taller" href="/liquidador/talleres/taller/taller"></BtnNavitagionSinister>
-            <BtnNavitagionSinister Icon={IconPricingComponent} text="Presupuesto" href="/liquidador/talleres/taller/presupuesto"></BtnNavitagionSinister>
-          </ContainerMenuTop>
+          <MenuTaller></MenuTaller>
 
           <TitlePresupuesto><h1>Taller Protuerca</h1></TitlePresupuesto>
 
         </Content>
-        
+
       </div>
 
     </Container>
