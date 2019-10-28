5a43bf3fba65a6d1d111a2ed978d9e343ab3e935
Jon Dotsoy <hi@jon.soy>
2019-10-28 00:58:20 -0300

    Update graph inicio liquidador

--- libs/components/panels/MetricsCar.component.tsx
+++ libs/components/panels/MetricsCar.component.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { PanelContainer, PanelHeader, PanelBody } from '../Panel';
 import styled from 'styled-components';
 import * as d3 from 'd3';
+import { ResponsiveBar, Bar } from '@nivo/bar';
 
 declare global {
   namespace NodeJS {
@@ -27,39 +28,68 @@ const ImgGraph = styled.img`
 
 const Graph = ({ ...opts }) => <ImgGraph {...opts} src="/static/graphBar.png"></ImgGraph>
 
-const GraphSvg = ({ ...opts}) => {
-  const refSvg = useRef<SVGSVGElement>(null);
+const rangeA = '$1 a $80.000';
+const rangeB = '$80.001 a $130.000';
+const rangeC = '$130.001 a $250.000';
+const rangeD = 'mas de $250.001';
 
-  const initializeGraphSvg = (svg: SVGSVGElement) => {
-    const selector = d3.select(svg);
+const sampleData = [
+  {
+    taller: 'Sin Golpes',
+    [rangeA]: 60,
+    [rangeB]: 50,
+    [rangeC]: 40,
+    [rangeD]: 30,
+  },
+  {
+    taller: 'Ratoncito',
+    [rangeA]: 30,
+    [rangeB]: 50,
+    [rangeC]: 20,
+    [rangeD]: 30,
+  },
+  {
+    taller: 'La Rueda',
+    [rangeA]: 70,
+    [rangeB]: 60,
+    [rangeC]: 50,
+    [rangeD]: 40,
+  },
+];
 
-    global.selector = selector;
+export default ({ ...props }) => {
+  const refPanel = useRef<HTMLDivElement>(null);
 
-    const g1 = selector.append('g');
-
-    g1.append('circle').attr('cx', 30).attr('cy', 30).attr('r', 6).style('fill', 'blue');
-
-    console.log(svg.outerHTML);
-  }
-
-  useEffect(() => {
-    if (refSvg.current) {
-      const svg = refSvg.current;
-
-      initializeGraphSvg(svg);
-    }
-  }, [refSvg.current]);
-
-  return <SVGGraph {...opts} ref={refSvg}></SVGGraph>;
-}
-
-export default ({ ...props }) =>
-  <PanelContainer {...props}>
+  return <PanelContainer {...props}>
     <PanelHeader>
       <h1>Deducible y tiempo entrega</h1>
       <h2>Deducible v/s tiempo</h2>
     </PanelHeader>
-    <PanelBody>
-      <Graph></Graph>
+    <PanelBody ref={refPanel}>
+      <Bar
+        height={300}
+        width={refPanel.current ? refPanel.current.offsetWidth : 440}
+        data={sampleData}
+        keys={[
+          rangeA,
+          rangeB,
+          rangeC,
+          rangeD,
+        ]}
+        indexBy={"taller"}
+        groupMode={"grouped"}
+        colors={{ scheme: 'oranges' }}
+        margin={{ top: 10, right: 10, bottom: 30, left: 30 }}
+        // legends={[
+        //   {
+        //     dataFrom: 'keys',
+        //     anchor: 'bottom-right',
+        //     direction: 'column',
+        //     itemWidth: -10,
+        //     itemHeight: 20,
+        //   }
+        // ]}
+      />
     </PanelBody>
-  </PanelContainer>
+  </PanelContainer>;
+}
