06cceea0fa535a4e5a482b50ca865597dbaedc9f
Jon Dotsoy <hi@jon.soy>
2019-11-10 12:05:34 -0300

    update  menu notificaciones

--- libs/components/Navbar/NotificationBox.tsx
+++ libs/components/Navbar/NotificationBox.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ItemWithImmg = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: -20px;
+  .icon-img {
+    display: inline-block;
+    width: 91px;
+    height: 40px;
+    background-position: center;
+    background-size: auto 164%;
+  }
+  .value-text {
+    flex: 1;
+  }
+  .value-result {
+    font-size: 1.4rem;
+    text-align: right;
+  }
+`;
 
 export const NotificationBox = () => <div className="notification-box">
   <div className="item-content header">
     <div className="text">
-      Notificaciones: 4
+      Notificaciones
     </div>
     <button>ver todos</button>
   </div>
 
   <div className="list-detail">
     <div className="item-content">
-      <div className="text">Nuevo presupuesto: 4</div>
+      <ItemWithImmg className="text">
+        <span className="icon-img" style={{ backgroundImage: 'url("/static/icono_notificaciones_-_1.png")' }} />
+        <div className="value-text">Nuevo presupuesto:</div>
+        <span className="value-result">4</span>
+      </ItemWithImmg>
     </div>
     <div className="item-content">
-      <div className="text">Sinistros Asignados: 20</div>
+      <ItemWithImmg className="text">
+        <span className="icon-img" style={{ backgroundImage: 'url("/static/icono_notificaciones_-_2.png")' }} />
+        <div className="value-text">Sinistros Asignados:</div>
+        <span className="value-result">20</span>
+      </ItemWithImmg>
     </div>
     <div className="item-content">
-      <div className="text">Presupuesto sin revisar: 2</div>
+      <ItemWithImmg className="text">
+        <span className="icon-img" style={{ backgroundImage: 'url("/static/icono_notificaciones_-_3.png")' }} />
+        <div className="value-text">Presupuesto sin revisar:</div>
+        <span className="value-result">2</span>
+      </ItemWithImmg>
     </div>
     <div className="item-content">
-      <div className="text">Vehiculo: VG3456 - Despachado</div>
+      <ItemWithImmg className="text">
+        <span className="icon-img" style={{ backgroundImage: 'url("/static/icono_notificaciones_-_auto.png")' }} />
+        <div className="value-text">Vehiculo:</div>
+        <span className="value-result">VG3456 - Despachado</span>
+      </ItemWithImmg>
     </div>
   </div>
 </div>;
